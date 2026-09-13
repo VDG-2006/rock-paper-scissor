8fa86f199c73448df194af1c443535719f41d71f
VDG-2006 <239400407+VDG-2006@users.noreply.github.com>
2026-09-13 03:02:46 +0000

    written script for 3 buttons and finished implementation

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
-
 let userScore=0;
 let computerScore=0;
+
+const play_content=document.querySelector(".hidden");
+play_content.style.display="none";
+
 let play=function(){
-    let userChoice=prompt("Enter rock,paper,scissor:").toLowerCase().trim();
-    let computerChoices=['rock','paper','scissor']
-    let computerChoice=Math.floor(Math.random()*3);
-    console.log(userChoice+" "+computerChoices[computerChoice]);
-    if(computerChoice===0){
-        if(userChoice==='paper') computerScore++;
-        else if(userChoice==='scissor') userScore++;
-    } else if(computerChoice===1){
-        if(userChoice==='rock') computerScore++;
-        else if(userChoice==='scissor') userScore++;
-    } else {
-        if(userChoice==='paper') computerScore++;
-        else if(userChoice==='rock') userScore++;
+    let choice_arr=['Rock','Paper','Scissor'];
+    let computer_choice=choice_arr[Math.floor(Math.random()*3)];
+
+    play_content.style.display="block";
+
+    const rock_btn=document.querySelector(".rock");
+    const paper_btn=document.querySelector(".paper");
+    const scissor_btn=document.querySelector(".scissor");
+
+    function updateInfo(){
+        document.querySelector(".computer-choice").textContent="Computer Choice: "+computer_choice;
+
+        document.querySelector("#user").textContent=userScore;
+        document.querySelector("#computer").textContent=computerScore;
+
+        play_content.style.display="none";
     }
-    document.getElementById("user").textContent=userScore;
-    document.getElementById("computer").textContent=computerScore;
+
+    rock_btn.onclick=  ()=>{
+        if(computer_choice==='Paper'){
+            computerScore++;
+        } else if(computer_choice==='Scissor') {
+            userScore++;
+        }
+        updateInfo();
+    };
+
+    paper_btn.onclick=  ()=>{
+        if(computer_choice==='Scissor'){
+            computerScore++;
+        } else if(computer_choice==='Rock') {
+            userScore++;
+        }
+        updateInfo();
+    };
+
+    scissor_btn.onclick=  ()=>{
+        if(computer_choice==='Rock'){
+            computerScore++;
+        } else if(computer_choice==='Paper') {
+            userScore++;
+        }
+        updateInfo();
+    };
+
 }
 document.getElementById("btn").addEventListener("click",play);
